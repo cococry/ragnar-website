3b1a704088b9b76978a38981266ff6c0454c3848
Cococry <cococry.btw@gmail.com>
2023-06-22 08:24:00 +0200

    Keybinds

--- app/keybinds/page.tsx
+++ app/keybinds/page.tsx
@@ -2,14 +2,24 @@
 const Keybinds = () => {
     return (
         <div className="px-6 md:px-40">
-            <h1 className="text-6xl bg-clip-text text-transparent bg-gradient-to-b from-[#fff] to-[#454545] mt-20 text-center font-bold">Ragnar Keybindings</h1>
+            <h1 className="text-6xl bg-clip-text text-transparent bg-gradient-to-b from-[#fff] to-[#454545] mt-20 mb-10 text-center font-bold">Ragnar Keybindings</h1>
 
             <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
-                { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1].map((e, i) => {
+                { [{desc: "Move Window", key: "Super + Left-click"},
+                    {desc: "Resize Window", key: "Super + Right-click"},
+                    {desc: "Hard-select Window", key: "Super + Middle-click"},
+                    {desc: "Move Window", key: "Super + Enter"},
+                    {desc: "Move Window", key: "Super + Enter"},
+                    {desc: "Move Window", key: "Super + Enter"},
+                    {desc: "Move Window", key: "Super + Enter"},
+      ].map((e) => {
                     return (
-                        <div className="h-10 bg-white">
-                            {e}
-                        </div>  
+                        <div className="flex flex-col justify-center items-center">
+                            <p className="font-bold text-2xl text-center text-white">{e.desc}</p>
+                            <div className="flex flex-col justify-center items-center font-bold text-center w-[200px] h-[75px] mt-2 bg-white shadow-lg shadow-pink-500/50 bg-gradient-to-b from-[#e8e8e8] to-[#f2c6f5]">
+                                {e.key}
+                            </div>
+                        </div>
                     )
                 })}
                 <div>
